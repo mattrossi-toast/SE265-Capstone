009c736e3339eab2375165a72cd062f36d3c08f9
Rossi <mtrossi@labs.neit.edu>
2018-08-07 19:25:08 -0400

    Updated styling before adding chartJS

--- views/tracker.php
+++ views/tracker.php
@@ -1,20 +1,21 @@
 
 <div>
-<h1>  <?php echo date("m/d/y"); ?> </h1>
+    <div class="tracker-head">
+<h1>  <?php echo "It is " . date("l, jS \of F Y"); ?> </h1>
 <h4> Your Template: </h4>
+    </div>
 
 <?php for( $i = 0; $i < 2; $i++){ ?>
-    <div class="goal-template">
-        <h1> Goal Text</h1>
-            <div class=".answer .answer-one">
+        <div class="question-container row">
+            <div class="answer answer-one col-lg-2 col-md-2 col-sm-2">
             </div>
-            <div class=".answer .answer-two">
+            <div class="answer answer-two col-lg-2 col-md-2 col-sm-2">
             </div>
-            <div class=".answer .answer-three">
+            <div class="answer answer-three col-lg-2 col-md-2 col-sm-2">
             </div>
-            <div class=".answer .answer-four">
+            <div class="answer answer-four col-lg-2 col-md-2 col-sm-2">
             </div>
-            <div class=".answer .answer-five">
+            <div class="answer answer-five col-lg-2 col-md-2 col-sm-2">
             </div>
     </div>
  <?php   }?>
