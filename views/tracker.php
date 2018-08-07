f1c69f5cb161c4550df70b95b38969aad98342bd
Rossi <mtrossi@labs.neit.edu>
2018-08-07 10:17:58 -0400

    First Commit

---
+++ views/tracker.php
@@ -0,0 +1,21 @@
+
+<div>
+<h1>  <?php echo date("m/d/y"); ?> </h1>
+<h4> Your Template: </h4>
+
+<?php for( $i = 0; $i < 2; $i++){ ?>
+    <div class="goal-template">
+        <h1> Goal Text</h1>
+            <div class=".answer .answer-one">
+            </div>
+            <div class=".answer .answer-two">
+            </div>
+            <div class=".answer .answer-three">
+            </div>
+            <div class=".answer .answer-four">
+            </div>
+            <div class=".answer .answer-five">
+            </div>
+    </div>
+ <?php   }?>
+</div>
